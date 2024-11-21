1ae26c92052508fa8e9bea3a981c387488eed895
Mendaka <chordtai@gmail.com>
2024-11-21 22:58:25 +0700

    ข้อความอธิบายเกี่ยวกับการเปลี่ยนแปลง

--- lib/add_transaction_form.dart
+++ lib/add_transaction_form.dart
@@ -3,7 +3,7 @@ import 'package:family_finance/language/locale_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:intl/intl.dart';
+
 import 'package:provider/provider.dart';
 
 class AddTransactionForm extends StatefulWidget {
--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'dashboard.dart';
 
 Future<bool> checkLoginStatus() async {
@@ -40,7 +39,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
-    return MaterialApp(
+    return 
+    MaterialApp(
       title: 'Family Finance',
      theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
